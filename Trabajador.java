9ab5b4664e7874fc1a0e6e1d392db974648aea07
Hernandezpilar <cuh51239922@cuh.mx>
2024-07-24 14:28:49 -0600

    Trabajador.java

---
+++ Trabajador.java
@@ -0,0 +1,13 @@
+package mx.com.cuh;
+
+public interface Trabajador {
+	
+	
+	//definimos los metodos para todos los trabajadores
+	public void checarEntrada();
+    public void trabajar();
+    public void horaComida();
+    public void cobrar();
+    public void checarSalida();
+
+}
